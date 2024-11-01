92da88b4bf4fbd51971f13eaea0c1f347973d540
Pome <71902427+1pome@users.noreply.github.com>
2024-11-01 12:10:10 -0300

    update folders

--- docs/js/script.js
+++ docs/js/script.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Cargar comandos desde JSON
-  fetch('../../assets/data/comandos.json')
+  fetch('./assets/data/comandos.json')
     .then(response => {
       if (!response.ok) {
         throw new Error('Error al cargar el archivo JSON');
