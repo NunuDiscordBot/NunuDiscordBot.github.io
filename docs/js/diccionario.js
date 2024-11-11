6ddb97bfb3835b5e13d6e64b827cc36b300606f0
Pome <71902427+1pome@users.noreply.github.com>
2024-11-11 01:39:29 -0300

    a

---
+++ docs/js/diccionario.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("searchForm"); // Usamos el ID del formulario directamente
+  const searchBox = document.getElementById("searchBox");
+  const feedback = document.querySelector(".feedback");
+  const tableBody = document.getElementById("command-table-body");
+
+  // Búsqueda en la tabla
+  searchBox.addEventListener("input", (e) => {
+    const term = e.target.value.toLowerCase();
+    const rows = Array.from(tableBody.rows); // Convertir NodeList a Array
+    let resultCount = 0;
+
+    rows.forEach((row) => {
+      const cells = row.querySelectorAll('td:not([data-searchable="false"])');
+      let isFound = false;
+
+      cells.forEach((cell) => {
+        const value = cell.innerText.toLowerCase();
+        if (value.includes(term)) {
+          isFound = true;
+        }
+        cell.classList.toggle("is-match", term && value.includes(term));
+      });
+
+      row.classList.toggle("is-hidden", !isFound); // Oculta las filas que no coinciden
+      resultCount += isFound ? 1 : 0;
+    });
+
+    // Actualiza el mensaje de retroalimentación
+    feedback.textContent = term
+      ? resultCount === 1
+        ? "Showing 1 result"
+        : `Showing ${resultCount} results`
+      : null;
+  });
+
+  // Cargar diccionario desde JSON
+  fetch("./assets/data/diccionario.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error al cargar el archivo JSON");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((command) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${command.command}</td>
+            <td>${command.description}</td>
+          `;
+        tableBody.appendChild(row);
+      });
+    })
+    .catch((error) => {
+      console.error("Error al cargar el archivo JSON:", error);
+      feedback.textContent = "Error loading commands";
+    });
+});
